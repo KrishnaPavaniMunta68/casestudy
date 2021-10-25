ee51c9a927b221253b31fb2892cd8f17f6810c48
KrishnaPavaniMunta68 <93111827+KrishnaPavaniMunta68@users.noreply.github.com>
2021-10-25 16:09:51 +0530

    Add files via upload

---
+++ OnlineWallet.java
@@ -0,0 +1,13 @@
+package teammatch;
+
+import java.io.IOException;
+
+public class OnlineWallet extends OptionMenu {
+		public static void main(String[] args) throws IOException {
+			OptionMenu Options = new OptionMenu();
+
+			Options.getLogin();
+		}
+	}
+
+
